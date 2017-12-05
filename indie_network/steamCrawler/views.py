86a4e93f0e791d671e364a03a96dfa76c6f14a15
Paulo Sergio <paulosergios11@hotmail.com>
2017-12-05 15:06:01 -0300

    django templates

--- indie_network/steamCrawler/views.py
+++ indie_network/steamCrawler/views.py
@@ -9,13 +9,14 @@ from indie_network.authentication.models import SteamUser as user
 def index(request):
     for u in user.objects.all():
         if (u.is_authenticated):
-            return JsonResponse(SteamUser(u.steamid).asJson(), safe = False)
+            return render(request, 'steamCrawler/index.html', {'user': (SteamUser(u.steamid)).asJson()})
 
 def userInfos(request, steamid = ''):
     return JsonResponse(SteamUser(steamid).asJson())
 
 def userGames(request, steamid = ''):
-    return JsonResponse(SteamUser(steamid).formatedGamesList(), safe = False)
+    return JsonResponse(SteamUser(steamid).formatedGamesList())
+    #return render(request, 'steamCrawler/dashboard_user.html', {'user': SteamUser(steamid).asJson(), 'games': SteamUser(steamid).formatedGamesList()})
 
 def gameInfos(request, appid = ""):
     return JsonResponse(SteamGame(appid).gameInfos, safe = False)
